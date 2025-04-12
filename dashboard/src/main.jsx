4b757d39e0cc36cdda661e0c802a5c8776a8a4cc
Parth Bhovad <parthbhovad710@gmail.com>
2025-04-12 21:21:45 +0530

    login, signup, add-funds, withdraw-funds route added

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import AddFunds from "./components/AddFunds";
+import WithdrawFunds from "./components/WithdrawFunds";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,6 +14,10 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/add-funds" element={<AddFunds />} />
+        <Route path="/withdraw-funds" element={<WithdrawFunds />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
