eca8386acee58b659f5d7d1dce9d185cb48a8ec3
Parth Bhovad <parthbhovad710@gmail.com>
2025-04-12 21:29:35 +0530

    UserModel added

---
+++ backend/model/UserModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+// User schema
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    balance: {
+        type: Number,
+        default: 0
+    },
+    holdings: [{
+        name: { type: String },
+        qty: { type: Number },
+        avg: { type: Number },
+        price: { type: Number },
+        net: { type: String },
+        day: { type: String },
+    }], // Embedded documents
+    orders: [{ type: Schema.Types.ObjectId, ref: "Order" }], // Referenced
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
