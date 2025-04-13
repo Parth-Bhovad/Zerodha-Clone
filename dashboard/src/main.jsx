cf8df770b0830aabff4ca17aeff1b1608556a887
Parth Bhovad <parthbhovad710@gmail.com>
2025-04-13 21:24:07 +0530

    Restructuring routes for implementing auth

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -9,18 +9,31 @@ import AddFunds from "./components/AddFunds";
 import WithdrawFunds from "./components/WithdrawFunds";
 import Profile from "./components/Profile";
 
+//AuthProvider
+import { AuthProvider } from "./AuthContext";
+
+//ProtectedRoutes
+import ProtectedRoute from "./ProtectedRoute";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
+  <AuthProvider>
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<Home />} />
+        {/* Public Routes (No auth required) */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/add-funds" element={<AddFunds />} />
-        <Route path="/withdraw-funds" element={<WithdrawFunds />} />
-        <Route path="/profile" element={<Profile />} />
+
+        {/* Protected Routes (User must be logged in) */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/*" element={<Home />} />
+          <Route path="/add-funds" element={<AddFunds />} />
+          <Route path="/withdraw-funds" element={<WithdrawFunds />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
+  </AuthProvider>
   // {/* </React.StrictMode> */}
 );
