6cd987e70d73009482bf31332d3dc2acf27c04e8
Parth Bhovad <parthbhovad710@gmail.com>
2025-04-16 21:49:31 +0530

    errors in development resolved. Testing production

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,13 @@ const allowedOrigins = process.env.NODE_ENV === "production" ?
         'http://localhost:5173',
     ];
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
 }));
 app.use(express.json());
